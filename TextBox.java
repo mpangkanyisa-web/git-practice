510823a80adeff79f4d1a753bc8c0ddc8661a2e3
Kanyisa <mpangkanyisa@gmail.com>
2026-08-19 19:40:38 +0200

    Textbox testing

---
+++ TextBox.java
@@ -0,0 +1,15 @@
+public class TextBox {
+    public static void main(String [] args){
+        String names = """
+                My name is Slim
+                Surname is Willz
+                """;
+
+
+                //if you put the lines next to the last line it doesn't leave an extra new line at the end , but if you put the """ ryt  at the bottom then you will get an extra new line.
+
+
+        System.out.println(names);
+    }
+
+}
