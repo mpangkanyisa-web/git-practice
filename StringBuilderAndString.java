9dd4be1a40567452b359c77fcd8db038eb71a385
Kanyisa <mpangkanyisa@gmail.com>
2026-08-19 07:42:48 +0200

    StringBuilderExample

---
+++ StringBuilderAndString.java
@@ -0,0 +1,15 @@
+public class StringBuilderAndString {
+    public static void main(String[] args) {
+        StringBuilder name1 = new StringBuilder("Kanyisa");
+        System.out.println(name1);
+
+        //or just:
+        // StringBuilder name1 = new StringBuilder();
+        // name1.append("Kannyisa"); 
+        // name1.insert(2, "Kay")
+        // name1.reverse();
+        // name1.replace(start, end, str)
+
+
+    }
+}
