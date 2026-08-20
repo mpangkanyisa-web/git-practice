d086cf068b49d668d31b7d94f32e9ecb41241b89
Kanyisa <mpangkanyisa@gmail.com>
2026-08-20 09:50:05 +0200

    IncrementAndPrinting rules in loops

---
+++ While.java
@@ -0,0 +1,12 @@
+public class While {
+    public static void main(String[] args){
+        int i = 0;
+        while (i < 5){
+             i++;
+            System.out.println(i);
+           
+        }
+    }
+}
+//for loop always increment after printing, and a while loop depends on where you put the increment
+
