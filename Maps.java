b59309a32f29f17f59da4b8d188baf96920ae40e
Kanyisa <mpangkanyisa@gmail.com>
2026-08-21 14:11:07 +0200

    maps keyset and value

---
+++ Maps.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+public class Maps {
+    public static void main(String[] args) {
+        Map<String, Integer> names = Map.of("Fred", 5, "Sheila", 5);
+        for(String f : names.keySet()){
+            System.out.println(f);
+        }
+        for(int n : names.values()){
+            System.out.println(n);
+        }
+    }
+}
