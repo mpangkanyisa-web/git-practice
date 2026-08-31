c884630b38a1370587a5b474aefbe7ef4a0051fc
Kanyisa <mpangkanyisa@gmail.com>
2026-08-31 08:04:06 +0200

    the constructor trick

--- X.java
+++ X.java
@@ -2,16 +2,16 @@ abstract  class Y {
     int x = 100;
     Y(int v) {
         x=v;
-        announce();   
+        // announce();   
     }
     abstract void announce();
 }
 public class X extends Y {
-    int x = 300;
+    // int x = 300;
     X() {super(200); }
     void announce() {System.out.println("x is " + x);}
     public static void main(String[] args) {
-        new X();
+        new X().announce();
     }
 }
 
