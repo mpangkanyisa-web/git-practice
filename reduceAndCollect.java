5378121ff4102cc42fc38df32cbd688be2cb9946
Kanyisa <mpangkanyisa@gmail.com>
2026-09-06 19:41:48 +0200

    partitionBy

--- reduceAndCollect.java
+++ reduceAndCollect.java
@@ -1,7 +1,7 @@
 
 import java.util.List;
 
-public class reduceAndCollect {
+public class ReduceAndCollect {
     public static void main(String[] args){
         List<Integer> numbers = List.of(1,2,3,4,5);
         int sum = numbers.stream().reduce(0, (subtotal, element) -> subtotal + element);
