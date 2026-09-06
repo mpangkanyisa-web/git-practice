f75f9d8be039ae37a8bed2ce64dc5c37c322fa7b
Kanyisa <mpangkanyisa@gmail.com>
2026-09-06 18:27:57 +0200

    reduce()

---
+++ reduceAndCollect.java
@@ -0,0 +1,9 @@
+
+import java.util.List;
+
+public class reduceAndCollect {
+    public static void main(String[] args){
+        List<Integer> numbers = List.of(1,2,3,4,5);
+        int sum = numbers.stream().reduce(0, (subtotal, element) -> subtotal + element);
+    }
+}
