ad34ca101a0374652bd5756781d4a8c2b4690e72
Kanyisa <mpangkanyisa@gmail.com>
2026-09-06 19:36:18 +0200

    GroupbyCollletcors

---
+++ CollectorsGroupBy.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+record Employee(String name, String department){}
+
+public class CollectorsGroupBy {
+    public static void main(String[] args) {
+        List<Employee> staff = List.of(
+            new Employee ("Alice", "HR"), 
+            new Employee("Bob", "IT"),
+            new Employee("Charlie", "IT")
+        );
+
+        //Grouping by Department
+
+        Map<String, List<Employee>> byDept = staff.stream().collect(Collectors.groupingBy(Employee::department));
+
+        System.out.println(byDept);
+
+    }
+}
